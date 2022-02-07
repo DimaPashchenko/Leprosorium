b1b83cf4e9bf5b123a48ebfb0d0783fcf8dca717
DimaPashchenko <kitaminchik@gmail.com>
2022-02-07 17:21:36 +0200

    Add texarea to /details/...

--- app.rb
+++ app.rb
@@ -59,10 +59,13 @@ end
 
 #вывод информации о посте
 get '/details/:post_id' do
+	#получаем переменную из url'a
 	post_id	= params[:post_id]
-
+	#получаем список постов
+	#(у нас будет только один пост)
 	results = @db.execute 'select * from Posts where id =?', [post_id]
-	@row = results[0]
-
+	#выбираем єтот один пост в переменную @row
+ 	@row = results[0]
+ 	#возвращаем представление details.erb
 	erb :details
 end
